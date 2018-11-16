6f7f3c3d9df025e7d757f30385176ab801269d9d
Atan1988 <tzw0423@hotmail.com>
2018-11-16 16:51:56 -0500

    commit

---
+++ R/create_tidysimloss_obj.R
@@ -0,0 +1,11 @@
+#' @title create a tidysimloss object
+#' @name crt_tidysimloss
+#' @description this function creates a tidysimloss object, based on a policy listing, as well as a claims transaction list
+#' @param policy_df
+#' @param claims_df
+#' @param policy_required_field_map
+#' @param claims_required_field_map
+#' @export
+crt_tidysimloss  <- function(policy_df, claims_df, policy_required_field_map, claims_required_field_map) {
+
+}
