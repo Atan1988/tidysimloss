775fcde4e57d935380e104d0e8f3c41f7efd7609
Atan1988 <tzw0423@hotmail.com>
2018-11-19 07:13:49 -0500

    commit

---
+++ inst/Overview.R
@@ -0,0 +1,17 @@
+# The claims simulation model has 3 major components
+#
+#initial State components:
+#{
+#ini_indemn_paid ~ ,
+#ini_expense_paid ~ ,
+#ini_indemn_reserve ~ ,
+#ini_expense_reserve ~
+#}
+#
+#transition state components:
+#{
+#closing ~ ,
+#reopen ~ ,
+#no_change ~ ,
+#
+#}
