6875d302aa821dd6cf89766235ff3e38f85a0702
Atan1988 <tzw0423@hotmail.com>
2018-11-22 12:12:17 -0500

    commit

--- R/expression_parser.R
+++ R/expression_parser.R
@@ -31,26 +31,33 @@ expr_evaluation <- function(df, expr_alist) {
   parsed_exprs <- expr_parser(expr_alist)
   data_list <- append(list(df = df), parsed_exprs$scalar_exprs)
 
-  ###evaluate equations first
-  for (i in 1:length(parsed_exprs$equation_exprs)) {
-    expr_var <- parsed_exprs$equation_exprs[[i]]
-    var <- names(parsed_exprs$equation_exprs)[i]
-    expr_to_eval <- 'df %>% dplyr::mutate(!!!expr_var)'
+  if (length(parsed_exprs$equation_exprs) > 0) {
+    ###evaluate equations first
+    for (i in 1:length(parsed_exprs$equation_exprs)) {
+      expr_var <- parsed_exprs$equation_exprs[[i]]
+      var <- names(parsed_exprs$equation_exprs)[i]
+      expr_to_eval <- 'df %>% dplyr::mutate(!!!expr_var)'
 
-    df <- rlang::eval_tidy(rlang::parse_expr(expr_to_eval), data = data_list)
-    colnames(df)[length(colnames(df))] <- var
-    data_list$df <- df
+      df <- rlang::eval_tidy(rlang::parse_expr(expr_to_eval), data = data_list)
+      colnames(df)[length(colnames(df))] <- var
+      data_list$df <- df
+    }
   }
 
   ###evaluate simulation
   for (i in 1:length(parsed_exprs$formula_exprs)) {
     formula_tmp <- parsed_exprs$formula_exprs %>% .[[i]] %>% as.list()
     dep <- formula_tmp[[2]]
-    RHS <- formula_tmp[[3]] %>% as.character()
+    RHS <- formula_tmp[[3]] %>% as.list()
+    RHS <- ifelse(RHS %>% purrr::map(is.character), paste0("'", RHS, "'"), RHS)
+    RHS_name <- formula_tmp[[3]] %>% as.list() %>% names()
+    if (is.null(RHS_name)) RHS_name <- rep("", length(RHS))
+
     func <- RHS[[1]]
     args <- RHS[-1]
+    args <- ifelse(RHS_name[-1] == "", paste0(args, ' = ', args), paste0(RHS_name[-1], ' = ', args))
     simulation_func <- paste0(func, "(n = n(), ",
-                              paste( paste(args, args, sep = ' = ') , collapse = ", "), ")")
+                              paste( args , collapse = ", "), ")")
     expr_to_eval <- paste0('df %>% dplyr::mutate(', dep, " = ", simulation_func, ")")
     df <- rlang::eval_tidy(rlang::parse_expr(expr_to_eval), data = data_list)
     data_list$df <- df
--- inst/simulate_claims_df.R
+++ inst/simulate_claims_df.R
@@ -4,11 +4,17 @@ policy_required_field_map <- c('PolicyNo' = 'Policy_Number',
                                'ExpirationDate' = 'Exp_dt')
 frequency_alist <- alist(
   total_claims ~ rpois(lambda),
+  lambda = a + b[Industries] * log(Exposures),
+  a = 0.05,
+  b = c(0.1, 0.05, 0.025)
+)
+severity_init_components_alist <- alist(
   occurrence_lag ~ rdiscrete(options),
   report_lag ~ rtrunc(FUN = 'exp', Att=0, rTrunc = 5 * 365, rate),
   rate = 1 / 180,
   options = seq(1, 365, 1)
 )
-severity_init_components_alist
 severity_transit_components_alist
 severity_params_components_alist
+
+expr_evaluation(df = Policy_df, expr_alist = frequency_alist)
